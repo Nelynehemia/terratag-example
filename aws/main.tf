1a462f9e8d245b903bbfcfe10d550cff39bd8c1a
nely <nely@cloudify.co>
2022-03-06 14:56:17 +0200

    new

--- aws/main.tf
+++ aws/main.tf
@@ -1,12 +1,27 @@
-provider "aws" {
-  region  = "us-east-1"
-  alias   = "custom"
-  version = "~> 3.0"
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+      version = "~> 0.0.13"
+    }
+  }
 }
 
-resource "aws_s3_bucket" "bucket" {
-  provider = aws.custom
+provider "env0" {}
 
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
+data "env0_project" "default_project" {
+  name = "Default Organization Project"
+}
+
+resource "env0_template" "example" {
+  name        = "example"
+  description = "Example template"
+  repository  = "https://github.com/env0/templates"
+  path        = "aws/hello-world"
+}
+
+resource "env0_configuration_variable" "in_a_template" {
+  name        = "VARIABLE_NAME"
+  value       = "some value"
+  template_id = env0_template.tested1.id
 }
